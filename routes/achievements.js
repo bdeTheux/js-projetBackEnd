92676badf106a208424a7365b36933d7f60e6cc8
Samy <samy.alliche@student.vinci.be>
2020-12-07 15:58:16 +0100

    ajout de la route achievement + ajout de code pour achievements

--- routes/achievements.js
+++ routes/achievements.js
@@ -8,8 +8,20 @@ const User = require("../model/User.js");
 const jwtSecret = "jkjJ1235Ohno!";
 
 
-router.get("/", authorize, function (req, res, next) {
-    return res.json(Achievement.achievements);
+router.get("/victory", function (req, res, next) {
+    return res.json(Achievement.achievementsVictory);
+});
+
+router.get("/defeat", function (req, res, next) {
+    return res.json(Achievement.achievementsDefeat);
+});
+
+router.get("/game", function (req, res, next) {
+    return res.json(Achievement.achievementsGame);
+});
+
+router.get("/time", function (req, res, next) {
+    return res.json(Achievement.achievementsTime);
 });
 
 module.exports = router;
