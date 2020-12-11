c05a69739488a4176355991187f4e88be3987720
unknown <nina.heuzer@student.vinci.be>
2020-12-11 18:20:18 +0100

    commentaire

--- app.js
+++ app.js
@@ -25,8 +25,7 @@ app.use(function (req, res, next) {
 });
 
 // error handler
-app.use(function (err, req, res, next) {  
-  // render the error page
+app.use(function (err, req, res, next) { 
   res.status(err.status || 500);
   res.send(err.message);
 });
